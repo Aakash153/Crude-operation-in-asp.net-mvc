514f470cf5d7c6f72d942a9f68be8afa6e35a1a3
Aakash Madhukar Khurange <68744200+Aakash153@users.noreply.github.com>
2021-07-24 00:43:17 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(crudemvc.Startup))]
+namespace crudemvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
